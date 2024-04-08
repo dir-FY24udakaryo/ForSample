92d3c584d0da31058b6212c6125bcb2b1cab94db
RyoUdaka(宇髙凌生) <RyoUdaka()@Dirbato-1401x>
2024-04-08 16:12:11 +0900

    演習終了

--- ForSample/src/sample/ForSample.java
+++ ForSample/src/sample/ForSample.java
@@ -3,8 +3,9 @@ package sample;
 public class ForSample {
 
     public static void main(String[] args) {
-        // TODO 自動生成されたメソッド・スタブ
-
+        for (var i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
     }
 
 }
